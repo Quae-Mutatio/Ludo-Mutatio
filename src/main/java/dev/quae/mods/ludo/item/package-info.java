06fca835dfa2bc258c2df662b5e2977c0bc2c9c8
Luke Gilfoyle <boo122333@gmail.com>
2020-09-14 16:16:35 +0100

    Add missing package-info.java files.
    Signed-off-by: Luke Gilfoyle <boo122333@gmail.com>

---
+++ src/main/java/dev/quae/mods/ludo/item/package-info.java
@@ -0,0 +1,7 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package dev.quae.mods.ludo.item;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
---
+++ src/main/java/dev/quae/mods/ludo/recipe/package-info.java
@@ -0,0 +1,7 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package dev.quae.mods.ludo.recipe;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
