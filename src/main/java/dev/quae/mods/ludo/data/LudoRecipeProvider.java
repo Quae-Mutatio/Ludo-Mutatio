2a89c88a96085de4f992a80cc76d63e21d6bbee6
Luke Gilfoyle <boo122333@gmail.com>
2020-09-15 14:00:13 +0100

    Reduce the number of plates from recipe to 4
    Signed-off-by: Luke Gilfoyle <boo122333@gmail.com>

--- src/main/java/dev/quae/mods/ludo/data/LudoRecipeProvider.java
+++ src/main/java/dev/quae/mods/ludo/data/LudoRecipeProvider.java
@@ -22,63 +22,63 @@ public class LudoRecipeProvider extends RecipeProvider {
                 .addIngredient(Ludo.Items.SOFT_PEBBLE)
                 .build(consumer);
 
-        TwoHandedRecipeBuilder.create(Ludo.Items.OAK_WOODEN_PLATE, 8)
+        TwoHandedRecipeBuilder.create(Ludo.Items.OAK_WOODEN_PLATE, 4)
                 .addIngredient(Tags.Items.CHISEL)
                 .addIngredient(Items.OAK_PLANKS)
                 .build(consumer);
-        TwoHandedRecipeBuilder.create(Ludo.Items.BIRCH_WOODEN_PLATE, 8)
+        TwoHandedRecipeBuilder.create(Ludo.Items.BIRCH_WOODEN_PLATE, 4)
                 .addIngredient(Tags.Items.CHISEL)
                 .addIngredient(Items.BIRCH_PLANKS)
                 .build(consumer);
-        TwoHandedRecipeBuilder.create(Ludo.Items.SPRUCE_WOODEN_PLATE, 8)
+        TwoHandedRecipeBuilder.create(Ludo.Items.SPRUCE_WOODEN_PLATE, 4)
                 .addIngredient(Tags.Items.CHISEL)
                 .addIngredient(Items.SPRUCE_PLANKS)
                 .build(consumer);
-        TwoHandedRecipeBuilder.create(Ludo.Items.JUNGLE_WOODEN_PLATE, 8)
+        TwoHandedRecipeBuilder.create(Ludo.Items.JUNGLE_WOODEN_PLATE, 4)
                 .addIngredient(Tags.Items.CHISEL)
                 .addIngredient(Items.JUNGLE_PLANKS)
                 .build(consumer);
-        TwoHandedRecipeBuilder.create(Ludo.Items.DARK_OAK_WOODEN_PLATE, 8)
+        TwoHandedRecipeBuilder.create(Ludo.Items.DARK_OAK_WOODEN_PLATE, 4)
                 .addIngredient(Tags.Items.CHISEL)
                 .addIngredient(Items.DARK_OAK_PLANKS)
                 .build(consumer);
-        TwoHandedRecipeBuilder.create(Ludo.Items.ACACIA_WOODEN_PLATE, 8)
+        TwoHandedRecipeBuilder.create(Ludo.Items.ACACIA_WOODEN_PLATE, 4)
                 .addIngredient(Tags.Items.CHISEL)
                 .addIngredient(Items.ACACIA_PLANKS)
                 .build(consumer);
-        TwoHandedRecipeBuilder.create(Ludo.Items.CRIMSON_WOODEN_PLATE, 8)
+        TwoHandedRecipeBuilder.create(Ludo.Items.CRIMSON_WOODEN_PLATE, 4)
                 .addIngredient(Tags.Items.CHISEL)
                 .addIngredient(Items.CRIMSON_PLANKS)
                 .build(consumer);
-        TwoHandedRecipeBuilder.create(Ludo.Items.WARPED_WOODEN_PLATE, 8)
+        TwoHandedRecipeBuilder.create(Ludo.Items.WARPED_WOODEN_PLATE, 4)
                 .addIngredient(Tags.Items.CHISEL)
                 .addIngredient(Items.WARPED_PLANKS)
                 .build(consumer);
-        TwoHandedRecipeBuilder.create(Ludo.Items.STONE_PLATE, 8)
+        TwoHandedRecipeBuilder.create(Ludo.Items.STONE_PLATE, 4)
                 .addIngredient(Tags.Items.CHISEL)
                 .addIngredient(Items.STONE)
                 .build(consumer);
-        TwoHandedRecipeBuilder.create(Ludo.Items.GRANITE_STONE_PLATE, 8)
+        TwoHandedRecipeBuilder.create(Ludo.Items.GRANITE_STONE_PLATE, 4)
                 .addIngredient(Tags.Items.CHISEL)
                 .addIngredient(Items.GRANITE)
                 .build(consumer);
-        TwoHandedRecipeBuilder.create(Ludo.Items.DIORITE_STONE_PLATE, 8)
+        TwoHandedRecipeBuilder.create(Ludo.Items.DIORITE_STONE_PLATE, 4)
                 .addIngredient(Tags.Items.CHISEL)
                 .addIngredient(Items.DIORITE)
                 .build(consumer);
-        TwoHandedRecipeBuilder.create(Ludo.Items.ANDESITE_STONE_PLATE, 8)
+        TwoHandedRecipeBuilder.create(Ludo.Items.ANDESITE_STONE_PLATE, 4)
                 .addIngredient(Tags.Items.CHISEL)
                 .addIngredient(Items.ANDESITE)
                 .build(consumer);
-        TwoHandedRecipeBuilder.create(Ludo.Items.SANDSTONE_PLATE, 8)
+        TwoHandedRecipeBuilder.create(Ludo.Items.SANDSTONE_PLATE, 4)
                 .addIngredient(Tags.Items.CHISEL)
                 .addIngredient(Items.SANDSTONE)
                 .build(consumer);
-        TwoHandedRecipeBuilder.create(Ludo.Items.RED_SANDSTONE_PLATE, 8)
+        TwoHandedRecipeBuilder.create(Ludo.Items.RED_SANDSTONE_PLATE, 4)
                 .addIngredient(Tags.Items.CHISEL)
                 .addIngredient(Items.RED_SANDSTONE)
                 .build(consumer);
-        TwoHandedRecipeBuilder.create(Ludo.Items.ENDSTONE_PLATE, 8)
+        TwoHandedRecipeBuilder.create(Ludo.Items.ENDSTONE_PLATE, 4)
                 .addIngredient(Tags.Items.CHISEL)
                 .addIngredient(Items.END_STONE)
                 .build(consumer);
